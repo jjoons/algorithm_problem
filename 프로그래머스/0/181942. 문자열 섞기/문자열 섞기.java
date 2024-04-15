629815674d4e26c4848a539fd1a794adc0b06cca
JS Lee <38220212+jjoons@users.noreply.github.com>
2024-04-15 21:22:11 +0900

    [level 0] Title: 문자열 섞기, Time: 11.94 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String[] a = str1.split("");
+        String[] b = str2.split("");
+        
+        String val = "";
+        
+        for (int i = 0; i < str1.length(); i++) {
+            val += a[i] + b[i];
+        }
+        
+        return val;
+    }
+}
