af7ef0253b3272a6e2571d626507f08a6577e673
JS Lee <38220212+jjoons@users.noreply.github.com>
2024-04-15 21:57:12 +0900

    [level 0] Title: 문자열 돌리기, Time: 134.39 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char[] ac = a.toCharArray();
+        
+        for (char c : ac) {
+            System.out.println(c);
+        }
+    }
+}
