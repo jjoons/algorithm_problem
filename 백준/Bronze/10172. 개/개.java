ef04a3bfda0079ae17b4e9d3c5ebb3b69eddb594
JS Lee <38220212+jjoons@users.noreply.github.com>
2024-07-15 19:12:52 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    private static <T> void println(T msg) {
+        System.out.println(msg);
+    }
+
+    public static void main(String... args) {
+        println("|\\_/|");
+        println("|q p|   /}");
+        println("( 0 )\"\"\"\\");
+        println("|\"^\"`    |");
+        println("||_/=\\\\__|");
+    }
+}
