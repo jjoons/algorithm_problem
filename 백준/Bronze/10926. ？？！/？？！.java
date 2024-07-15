ab2db9c45de622ae621bd8db2617a6d6958be452
JS Lee <38220212+jjoons@users.noreply.github.com>
2024-07-15 18:58:22 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    private static <T> void println(T msg) {
+        System.out.println(msg);
+    }
+
+    public static void main(String... args) {
+        var sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        println(a + "??!");
+    }
+}
