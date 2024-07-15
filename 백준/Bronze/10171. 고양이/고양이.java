b648956ac1efa4a17fd6cd63d8ea80595cd604dc
JS Lee <38220212+jjoons@users.noreply.github.com>
2024-07-15 19:11:03 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    private static <T> void println(T msg) {
+        System.out.println(msg);
+    }
+
+    public static void main(String... args) {
+        println("\\    /\\");
+        println(" )  ( ')");
+        println("(  /  )");
+        println(" \\(__)|");
+    }
+}
