c23aab22910c079c69905253291ce9936fc6a2f0
JS Lee <38220212+jjoons@users.noreply.github.com>
2025-02-17 20:56:16 +0900

    [Bronze V] Title: A×B, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        final int input1 = sc.nextInt();
+        final int input2 = sc.nextInt();
+
+        System.out.println(input1 * input2);
+    }
+}
