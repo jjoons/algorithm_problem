f175058afb1f3e6d30e6a34248567154bf4ac6ea
JS Lee <38220212+jjoons@users.noreply.github.com>
2025-02-17 22:11:50 +0900

    [Bronze V] Title: 윤년, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,18 +1,15 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    private static <T> void println(T msg) {
-        System.out.println(msg);
-    }
-    
-    public static boolean isLeapYear(int year) {
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    private static boolean isLeapYear(int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
 
-    public static void main(String... args) {
-        var sc = new Scanner(System.in);
-        int y = sc.nextInt();
-        
-        println(isLeapYear(y) ? 1 : 0);
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        final int year = sc.nextInt();
+
+        final boolean leapYear = isLeapYear(year);
+        System.out.println(leapYear ? 1 : 0);
     }
 }
