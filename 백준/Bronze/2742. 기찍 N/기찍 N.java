238984f00c9216a90fbc1c8bb1e27a3887b6356f
JS Lee <38220212+jjoons@users.noreply.github.com>
2025-02-19 03:26:04 +0900

    [Bronze IV] Title: 기찍 N, Time: 880 ms, Memory: 33892 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        final int count = sc.nextInt();
+
+        for (int i = count; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
