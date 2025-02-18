1059b8855cc0832fd08e5aae304e3ad3a1d42a9d
JS Lee <38220212+jjoons@users.noreply.github.com>
2025-02-18 20:27:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        final char input = sc.next().charAt(0);
+        System.out.println((byte) input);
+    }
+}
