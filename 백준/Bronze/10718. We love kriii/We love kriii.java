6c58747db65f9eb4e9d1f025d773785a19da64fa
JS Lee <38220212+jjoons@users.noreply.github.com>
2025-02-18 22:38:00 +0900

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        final String message = "강한친구 대한육군";
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println(message);
+        }
+    }
+}
