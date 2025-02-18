e27e410d37820fb328f09533c8aaaa411fa1f8e2
JS Lee <38220212+jjoons@users.noreply.github.com>
2025-02-19 03:18:58 +0900

    [Bronze V] Title: 개수 세기, Time: 176 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,14 +6,18 @@ public class Main {
         final int totalCount = sc.nextInt();
         sc.nextLine();
 
-        final String arrStr = sc.nextLine();
-        final String[] arr = arrStr.split(" ");
-        final int search = sc.nextInt();
+        final String[] ints = sc.nextLine().split(" ");
+        final int searchNum = sc.nextInt();
+        int result = 0;
 
-        List<String> list = Arrays.stream(arr)
-            .filter(s -> search == Integer.parseInt(s))
-            .collect(Collectors.toList());
+        for (int i = 0; i < ints.length; i++) {
+            final int value = Integer.parseInt(ints[i]);
 
-        System.out.println(list.size());
+            if (value == searchNum) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
     }
 }
