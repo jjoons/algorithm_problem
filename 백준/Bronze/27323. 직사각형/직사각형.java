1e1c98f0cc4c08bc15d60cce2d0d0c7081de4f54
JS Lee <38220212+jjoons@users.noreply.github.com>
2025-02-20 01:43:24 +0900

    [Bronze V] Title: 직사각형, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        final int a = sc.nextInt();
+        final int b = sc.nextInt();
+
+        System.out.println(a * b);
+    }
+}
